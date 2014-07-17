505372779b8fecfeee4546b62a6077281b673ee9
intersail <info@intersail.it>
2014-07-17 11:27:07 +0200

    Namespace update. Settings as class.

--- lib/remote_models.rb
+++ lib/remote_models.rb
@@ -2,9 +2,9 @@ require "remote_models/version"
 require 'remote_models/concerns/models/remote_associable'
 require 'remote_models/concerns/models/remote_model'
 
-module RemoteModels
-  class Settings
-    @@config = {}
+module Intersail
+  module RemoteModels
+    @@config = Settings.new
 
     def self.setup(&block)
       yield @@config
@@ -12,6 +12,17 @@ module RemoteModels
 
     def self.config
       @@config
+    end
+
+    class Settings
+      @config = {}
+
+      def site
+        @config[:site]
+      end
+      def site=(value)
+        @config[:site] = value
+      end
     end
   end
 end
--- lib/remote_models/concerns/models/remote_associable.rb
+++ lib/remote_models/concerns/models/remote_associable.rb
@@ -10,7 +10,7 @@ module Intersail
 
         self.remote_fields = []
 
-        self.site = ::RemoteModels::Settings.config[:site]
+        self.site = ::Intersail::RemoteModels.config.site
       end
 
       module ClassMethods
