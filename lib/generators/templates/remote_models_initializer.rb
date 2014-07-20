78ef26a47a4a2d0b2f2084c24c0f5174db4cc277
Cristiano Boncompagni <cristiano.boncompagni@intersail.it>
2014-07-20 22:50:00 +0200

    Add geenrator for initializer file

---
+++ lib/generators/templates/remote_models_initializer.rb
@@ -0,0 +1,4 @@
+Intersail::RemoteModels.configure do |config|
+  # Set the URL of the remote service
+  # config.site = 'http://localhost/JsonService/ReadEntity.aspx'
+end
--- lib/settings.rb
+++ lib/settings.rb
@@ -1,16 +1,7 @@
 module Intersail
   module RemoteModels
     class Settings
-      def initialize
-        @config = {}
-      end
-
-      def site
-        @config[:site]
-      end
-      def site=(value)
-        @config[:site] = value
-      end
+      attr_accessor site
     end
   end
 end
